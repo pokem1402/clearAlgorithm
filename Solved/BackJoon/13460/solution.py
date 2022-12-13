63c1208ed34b62794cdf47378b2886473c25b670
unknown <pokem@naver.com>
2022-12-13 11:52:42 +0900

    modified backjoon_13460 comment

--- Solved/BackJoon/13460/solution.py
+++ Solved/BackJoon/13460/solution.py
@@ -1,19 +1,25 @@
 from collections import deque
 
-# Red ball 과 Blue Ball를 찾는 함수
-# 판을 입력하면 red ball의 y좌표와 x 좌표를 획득한다.
 RY, RX, BY, BX = 0, 1, 2, 3
 
+# Red ball 과 Blue Ball를 찾는 함수
+# board에서 공의 좌표를 획득한 뒤에, board에서부터 공을 지우는 함수
+# board에서 공을 지우는 이유는 bfs의 모든 과정에서 board를 재활용하기 위함이다.
+
+
 def find_RandB(board):
+	#공의 좌표는 적색 공의 좌표 y,x 그리고 청색 공의 y,x로 구성된다.
     ball = [-1, -1, -1, -1]
     for y, line in enumerate(board):
         if 'R' in line:
             x = line.index('R')
             ball[:2] = [y, x]
+            # 공을 보드로부터 지우기
             board[y][x] = '.'
         if 'B' in line:
             x = line.index('B')
             ball[2:] = [y, x]
+            # 공을 보드로부터 지우기
             board[y][x] = '.'
     return ball
 
@@ -22,6 +28,8 @@ def find_RandB(board):
 # 따라서 먼저 굴러갈 공을 먼저 굴리고나서 그 다음 공을 굴리면 된다.
 # 먼저 굴린 공은 같은 방향에서 늦게 굴린 공을 만나지 않고, 늦게 굴린 공은 먼저 굴린 공의 위치를 참고하므로
 # 다음 위치를 참조할 때는 빈 공간인가 아닌가만 확인하면 된다.
+
+
 def toward(board, ball, direction):
 
     # 빈 보드에 공을 놓는다.
@@ -39,7 +47,6 @@ def toward(board, ball, direction):
     red_on_hole = False
     blue_on_hole = False
 
-
     # 공과 방향에 대한 내적
     # 방향 벡터와의 내적 결과 값이 큰 쪽이 해당 벡터 방향으로 멀리 있는 쪽.
     # 멀리 있는 쪽이 이동 결과 같거나 더 먼쪽에 위치함
@@ -47,6 +54,7 @@ def toward(board, ball, direction):
     # 따라서 내적 값이 큰 공을 먼저 이동하고 그 다음 공을 이동하면됨
     # 정리하면 x1x2+y1y2이 큰 값을 먼저 이동하면 됨.
     # 반대로 코드에서는 -x1x2-y1y2이 작은 값을 먼저 이동시킴
+
     def dot(x, y): return - x[0]*y[0] - x[1]*y[1]
 
     # 내적의 결과 값이 작은 공을 먼저 굴리고, 나머지 공을 다음에 굴린다.
@@ -147,12 +155,11 @@ def bfs(board, ball_init):
                     visited.append(ball_next)
     else:
         return -1
-    
-    
+
+
 n, m = map(int, input().split())
 
 board = [list(input()) for _ in range(n)]
 
 ball_init = find_RandB(board)
 print(bfs(board, ball_init))
-
